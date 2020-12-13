655141a799e803f8012c6d6888504b8ce31a9e92
dbhutta00 <73953619+dbhutta00@users.noreply.github.com>
2020-12-13 18:57:45 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,27 @@
+# Task 1 
+
+from source import BOD5
+
+BOD5.loc['BOD_5'] = (BOD5.loc['DO_0'] - BOD5.loc['DO_5']) / BOD5.loc['Decimal_Dilution']
+
+print(BOD5.loc['BOD_5'])
+BOD5
+
+print(BOD_pri)
+print(BOD_pri)
+
+if BOD_sec > 25:
+  print('Final effluent:', BOD_sec, 'mg/L' ' , ' 'This facility is not in compliance with EPA Clean Water Act!',)
+else:
+  print('Final effluent:', BOD_sec, 'mg/L' ' , ' 'This facility is in compliance with EPA Clean Water Act.'
+
+print(percent_removal,'%')
+  
+# Task 2 
+
+from source import BOD_DO
+
+print(BOD_DO)
+plt.show()
+
+print('BOD_Ultimate:', bod_ultimate, 'mg/L')
