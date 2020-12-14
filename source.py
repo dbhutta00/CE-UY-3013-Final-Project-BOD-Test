e9e8874121301b285e65796915450a3493010606
dbhutta00 <73953619+dbhutta00@users.noreply.github.com>
2020-12-13 20:13:40 -0500

    Update source.py

--- source.py
+++ source.py
@@ -3,6 +3,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# TASK 1: 5-Day BOD Calculations
 
+# Insert your dataframe for Dissolved Oxygen at Day 0 and Day 5 per primary and secondary treatment 
+# Standard is three samples but can input more
+# Also input the decimal dilution of your samples 
+# 2% and 10% are standards used for primary and secondary effluent respectively
+# Can change decimal dilution if needed
+# PE = Primary Effluent and SE= Secondary
+
+BOD5 = pd.DataFrame({
+                   "PE1": [8.68, 1.44, 0.02],
+                   "PE2": [7.33, 1.02, 0.02],
+                   "PE3": [8.48, 1.49, 0.02],
+                   "SE1": [9.39, 5.35, 0.10],
+                   "SE2": [7.82, 2.38, 0.10],
+                   "SE3": [8.42, 4.07, 0.10]},
+                   index = ["DO_0", "DO_5", "Decimal_Dilution"])
+BOD5
 
 
